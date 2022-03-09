8e24c92ab1d9de23ce1a78b1e045542b1f2d1e99
Rahib107 <85207102+Rahib107@users.noreply.github.com>
2022-03-09 12:07:31 +0600

    Add files via upload

---
+++ file-io/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1> welcome</h1>
+	<br>
+	<br>
+
+	<a href="Logout.php">Logout</a>
+	 </form>
+
+</body>
+</html>
